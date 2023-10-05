89d9e6ad10c6f9c95e77ab2cb05c7ac71548cfdc
jp <wongjingping@gmail.com>
2023-10-05 19:08:13 +0800

    updated with embeddings, ner and join columns info

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="defog-data",
-    version="0.1.0",
+    version="0.1.1",
     description="Static SQL and JSON files containing the data we use for evaluations",
     author="Defog",
     author_email="support@defog.ai",
-    py_modules=["defog_data"],
+    py_modules=["defog_data", "gen_embeddings"],
     install_requires=[],
 )
