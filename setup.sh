4348e12506c2a6a0d4d386abe1c0dcd2032347c6
jp <wongjingping@gmail.com>
2023-10-09 15:31:57 +0800

    fix path in setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ for db_name in "$@"; do
     PGPASSWORD=$DBPASSWORD psql -U $DBUSER -h $DBHOST -p $DBPORT -c "DROP DATABASE IF EXISTS ${db_name};"
     PGPASSWORD=$DBPASSWORD psql -U $DBUSER -h $DBHOST -p $DBPORT -c "CREATE DATABASE ${db_name};"
     echo "done dropping and recreating database ${db_name}"
-    db_path="${db_name}/${db_name}.sql"
+    db_path="defog_data/${db_name}/${db_name}.sql"
     echo "importing ${db_path} into database ${db_name}"
     PGPASSWORD=$DBPASSWORD psql -U $DBUSER -h $DBHOST -p $DBPORT -d "${db_name}" -f "${db_path}"
 done
