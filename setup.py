2deb184f3a5d6a1a711185c79eec169d9669a899
jp <wongjingping@gmail.com>
2024-03-28 17:08:03 +0800

    add relative dates for customers.crtd_ts and sales.crtd_ts change date in example remove excess customers add rows to break tie for aggregate bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="defog-data",
-    version="0.1.1",
+    version="0.1.2",
     description="Static SQL and JSON files containing the data we use for evaluations",
     author="Defog",
     author_email="support@defog.ai",
